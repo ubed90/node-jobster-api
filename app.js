e0ab5f71b27f0f4d47c99ca6c1e2c7c98edd6de1
UbedS <shaikhobaid123@gmail.com>
2023-10-14 19:14:49 +0530

    ENABLED CORS FOR CROSS BACKEND

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-require('dotenv').config({ path: "./config/.env" });
-require('express-async-errors');
-
+require('dotenv').config({ path: './config/.env' })
+require('express-async-errors')
 
 // * Configuring CLient
-const path = require("path");
+const path = require('path')
 
 // extra security packages
-const helmet = require('helmet');
+const helmet = require('helmet')
 // ! Not required in Jobster
-// const cors = require('cors');
-const xss = require('xss-clean');
+const cors = require('cors')
+const xss = require('xss-clean')
 // ! Not required in Jobster
 // const rateLimiter = require('express-rate-limit');
 
@@ -19,17 +18,17 @@ const xss = require('xss-clean');
 // const YAML = require('yamljs');
 // const swaggerDocument = YAML.load('./swagger.yaml');
 
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-const connectDB = require('./db/connect');
-const authenticateUser = require('./middleware/authentication');
+const connectDB = require('./db/connect')
+const authenticateUser = require('./middleware/authentication')
 // routers
-const authRouter = require('./routes/auth');
-const jobsRouter = require('./routes/jobs');
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 // ! Not required in Jobster
 // app.set('trust proxy', 1);
@@ -41,13 +40,13 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 // );
 
 // FrontEnd
-app.set('trust proxy', 1);
-app.use(express.static(path.resolve(__dirname, './client/build')));
-app.use(express.json());
-app.use(helmet());
+app.set('trust proxy', 1)
+app.use(express.static(path.resolve(__dirname, './client/build')))
+app.use(express.json())
+app.use(helmet())
 // ! Not required in Jobster
-// app.use(cors());
-app.use(xss());
+app.use(cors())
+app.use(xss())
 
 // ! Not required in Jobster
 // app.get('/', (req, res) => {
@@ -56,28 +55,28 @@ app.use(xss());
 // app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
 // ! Serve index.html for every route other than API ROute
-app.get("*", (req,res) => {
-  return res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
-});
+app.get('*', (req, res) => {
+  return res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
 
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
-    );
+    )
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
-start();
+start()
